d4392ec4750a679d97a536d1c8bbf0de43c8019c
itsme-sachin <67556881+itsme-sachin@users.noreply.github.com>
2020-06-30 22:08:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ init();
 
 // btn();
 document.querySelector('.btn-roll').addEventListener('click',function(){
-    if(gamePlaying){
+  if(gamePlaying){ 
     // random no
     var dice=Math.floor(Math.random()*6)+1;
 
@@ -26,11 +26,11 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
       else{
           nextPlayer();
       }
-
-}});
+    }
+});
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
-    if(gamePlaying){
+  if(gamePlaying){ 
   // Add current score to global score
  score[activePlayer]+=roundScore;
 
@@ -38,21 +38,21 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
   document.querySelector('#score-'+activePlayer).textContent=score[activePlayer];
 
 // Winner
-if(score[activePlayer]>=100){
+if(score[activePlayer]>=20){
   document.querySelector('#name-'+activePlayer).textContent='Winner!!';
   document.querySelector('.dice').style.display='none';
   document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
   document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
-
+  gamePlaying=false;
 }
 else{
 // next player
 nextPlayer();
 }
 
+}
 
-
-}})
+})
 
 function nextPlayer(){
   activePlayer===0 ?activePlayer=1:activePlayer=0;
@@ -75,7 +75,7 @@ function init(){
   score=[0,0];
 roundScore=0;
 activePlayer=0;
-    gamePlaying=true;
+gamePlaying=true;
 document.querySelector('.dice').style.display='none';
 document.getElementById('score-0').textContent='0';
 document.getElementById('score-1').textContent='0';
@@ -90,9 +90,3 @@ document.querySelector('.player-0-panel').classList.remove('active');
 document.querySelector('.player-1-panel').classList.remove('active');
 document.querySelector('.player-0-panel').classList.add('active');
 }
-
-
-
-
-
-
