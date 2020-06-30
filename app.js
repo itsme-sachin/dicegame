64730f080a5209ddff5b954d0356470b19942fb8
itsme-sachin <67556881+itsme-sachin@users.noreply.github.com>
2020-06-30 22:01:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var score,roundScore,activePlayer;
+var score,roundScore,activePlayer,gamePlaying;
 
 init();
 // console.log(dice);
@@ -8,6 +8,7 @@ init();
 
 // btn();
 document.querySelector('.btn-roll').addEventListener('click',function(){
+    if(gamePlaying){
     // random no
     var dice=Math.floor(Math.random()*6)+1;
 
@@ -26,9 +27,10 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
           nextPlayer();
       }
 
-});
+}});
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
+    if(gamePlaying){
   // Add current score to global score
  score[activePlayer]+=roundScore;
 
@@ -36,7 +38,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
   document.querySelector('#score-'+activePlayer).textContent=score[activePlayer];
 
 // Winner
-if(score[activePlayer]>=20){
+if(score[activePlayer]>=100){
   document.querySelector('#name-'+activePlayer).textContent='Winner!!';
   document.querySelector('.dice').style.display='none';
   document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
@@ -50,7 +52,7 @@ nextPlayer();
 
 
 
-})
+}})
 
 function nextPlayer(){
   activePlayer===0 ?activePlayer=1:activePlayer=0;
@@ -73,6 +75,7 @@ function init(){
   score=[0,0];
 roundScore=0;
 activePlayer=0;
+    gamePlaying=true;
 document.querySelector('.dice').style.display='none';
 document.getElementById('score-0').textContent='0';
 document.getElementById('score-1').textContent='0';
